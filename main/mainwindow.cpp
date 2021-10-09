5e761ac0d574ce84daf8dc0bc5baaa94654ef8cc
まいまい <kengsini250@gmail.com>
2021-10-09 14:54:38 +0800

    Add files via upload

---
+++ main/mainwindow.cpp
@@ -0,0 +1,48 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "../pluginTest_1/add.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton_Load, &QPushButton::clicked, this, [this]
+            {
+                //加载dll
+                QLibrary dll("plugin/pluginTest_1.dll");
+
+                if (dll.load())
+                {
+
+                    //dll的函数入口
+                    //利用函数指针调用
+                    usedll = (UseDll)dll.resolve("func");
+
+                    if (!usedll)
+                    {
+                        qDebug() << "use : " << dll.errorString();
+                    }
+                }
+                else
+                {
+                    qDebug() << "load : " << dll.errorString();
+                }
+            });
+
+    connect(ui->pushButton, &QPushButton::clicked, this, [this]
+            {
+                QString a = ui->textEdit->toPlainText();
+                QString b = ui->textEdit_2->toPlainText();
+                int aa = a.toInt();
+                int bb = b.toInt();
+
+                //使用插件
+                ui->label_2->setText(QString::number(usedll(aa, bb)));
+            });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
